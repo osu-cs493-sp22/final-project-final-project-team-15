55e59ca8ccdf9fa24eb10ab1a9a38fdb88c6e7b0
NawafAlOthman <72106212+NawafAlOthman@users.noreply.github.com>
2022-06-04 15:25:53 -0700

    POST user added

--- api/users.js
+++ api/users.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-const { UserSchema} = require('../models/user')
-const {generateAuthToken, requireAuthentication} = require('../lib/auth')
-
+const { UserSchema, insertNewUser } = require("../models/users");
+const { generateAuthToken, requireAuthentication } = require("../lib/auth");
+const { validateAgainstSchema } = require("../lib/validation");
 
 exports.router = router;
 
@@ -12,68 +12,79 @@ exports.router = router;
 // const { reviews } = require('./reviews');
 // const { photos } = require('./photos');
 
-const { getDbInstance } = require('../lib/mongo')
+const { getDbInstance } = require("../lib/mongo");
 
 /*
  * Route to list all of a user's businesses.
  */
-router.get('/:userid/businesses', async function(req, res) {
-    const db = getDbInstance()
-    const collection = db.collection('businesses')
-    const businesses = await collection.find({}).toArray()
-    const userid = parseInt(req.params.userid);
-    const userBusinesses = businesses.filter(business => business && business.ownerid === userid);
+router.get("/:userid/businesses", async function (req, res) {
+  const db = getDbInstance();
+  const collection = db.collection("businesses");
+  const businesses = await collection.find({}).toArray();
+  const userid = parseInt(req.params.userid);
+  const userBusinesses = businesses.filter(
+    (business) => business && business.ownerid === userid
+  );
 
-    res.status(200).send({ businesses: userBusinesses })
+  res.status(200).send({ businesses: userBusinesses });
 });
 
 /*
  * Route to list all of a user's reviews.
  */
-router.get('/:userid/reviews', async function(req, res) {
-    const db = getDbInstance()
-    const collection = db.collection('reviews')
-    const reviews = await collection.find({}).toArray()
-    const userid = parseInt(req.params.userid);
-    const userReviews = reviews.filter(review => review && review.userid === userid);
-    res.status(200).send({
-        reviews: userReviews
-    });
+router.get("/:userid/reviews", async function (req, res) {
+  const db = getDbInstance();
+  const collection = db.collection("reviews");
+  const reviews = await collection.find({}).toArray();
+  const userid = parseInt(req.params.userid);
+  const userReviews = reviews.filter(
+    (review) => review && review.userid === userid
+  );
+  res.status(200).send({
+    reviews: userReviews,
+  });
 });
 
 /*
  * Route to list all of a user's photos.
  */
-router.get('/:userid/photos', async function(req, res) {
-    const db = getDbInstance()
-    const collection = db.collection('photos')
-    const photos = await collection.find({}).toArray()
-    const userid = parseInt(req.params.userid);
-    const userPhotos = photos.filter(photo => photo && photo.userid === userid);
-    res.status(200).send({
-        photos: userPhotos
-    });
+router.get("/:userid/photos", async function (req, res) {
+  const db = getDbInstance();
+  const collection = db.collection("photos");
+  const photos = await collection.find({}).toArray();
+  const userid = parseInt(req.params.userid);
+  const userPhotos = photos.filter((photo) => photo && photo.userid === userid);
+  res.status(200).send({
+    photos: userPhotos,
+  });
 });
 
-router.post('/login', async function (req, res) {
-    if (req.body && req.body.email && req.body.password){
-      const email = req.body.email
-      //const userpass = await User.findOne({where: { email: email }},'password')
-      const userpass = await User.find({ email: email })
-      const hashcheck = userpass && await bcrypt.compareSync(
-      req.body.password,
-      userpass.password
-      )
-      if (hashcheck){
-        const token = generateAuthToken(userpass.userId, userpass.admin)
-        res.status(200).send({
-          token: token
-        })
-      }
-      else 
-        res.status(401).send({error :"error invalid credentials"})
-    }
-    else 
-        res.status(401).send({error :"error invalid request body"})
-    })
-    
+router.post("/", async function (req, res) {
+  if (validateAgainstSchema(req.body, UserSchema)) {
+    const id = await insertNewUser(req.body);
+    res.status(201).send({
+      _id: id,
+    });
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User.",
+    });
+  }
+});
+
+router.post("/login", async function (req, res) {
+  if (req.body && req.body.email && req.body.password) {
+    const email = req.body.email;
+    //const userpass = await User.findOne({where: { email: email }},'password')
+    const userpass = await User.find({ email: email });
+    const hashcheck =
+      userpass &&
+      (await bcrypt.compareSync(req.body.password, userpass.password));
+    if (hashcheck) {
+      const token = generateAuthToken(userpass.userId, userpass.admin);
+      res.status(200).send({
+        token: token,
+      });
+    } else res.status(401).send({ error: "error invalid credentials" });
+  } else res.status(401).send({ error: "error invalid request body" });
+});
--- models/users.js
+++ models/users.js
@@ -1,3 +1,9 @@
+const { ObjectId } = require("mongodb");
+const bcrypt = require("bcryptjs");
+
+const { extractValidFields } = require("../lib/validation");
+const { getDbInstance } = require("../lib/mongo");
+
 const UserSchema = {
   name: { required: true },
   email: { required: true },
@@ -5,3 +11,13 @@ const UserSchema = {
   role: { required: true },
 };
 exports.UserSchema = UserSchema;
+
+exports.insertNewUser = async function (user) {
+  const userToInsert = extractValidFields(user, UserSchema);
+  userToInsert.password = await bcrypt.hash(userToInsert.password, 8);
+  console.log("== Hashed, salted password:", userToInsert.password);
+  const db = getDbInstance();
+  const collection = db.collection("users");
+  const result = await collection.insertOne(userToInsert);
+  return result.insertedId;
+};
