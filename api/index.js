a8fffb33c49cfa3f1e5b85d1e192c644d94ffc1c
NawafAlOthman <72106212+NawafAlOthman@users.noreply.github.com>
2022-06-04 14:45:26 -0700

    updated api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
-const router = module.exports = require('express').Router();
+const router = (module.exports = require("express").Router());
 
-router.use('/businesses', require('./businesses').router);
-router.use('/reviews', require('./reviews').router);
-router.use('/photos', require('./photos').router);
-router.use('/users', require('./users').router);
+router.use("/businesses", require("./businesses").router);
+router.use("/reviews", require("./reviews").router);
+router.use("/photos", require("./photos").router);
+router.use("/users", require("./users").router);
+router.use("/assignments", require("./assignments").router);
+router.use("/courses", require("./courses").router);
