35d7b676b9c952c4a83066fa4dc2acb44f61482f
Hunteret7 <hunteret@oregonstate.edu>
2022-06-04 14:55:36 -0700

    courses api points

---
+++ api/courses.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { validateAgainstSchema, extractValidFields } = require('../lib/validation');
+
+const CourseSchema = require('../models/courses')
+const { users } = require('./users');
+const { assignments } = require('./assingments');
+const { submissions } = require('./submissions');
+
+const { getDbInstance } = require('../lib/mongo')
+
+const { ObjectID, ListCollectionsCursor, ObjectId } = require('mongodb');
+const e = require('express');
+
+exports.router = router;
+exports.courses = courses;
+
+
+router.get('/', async(req, res) => {
+
+});
+
+router.post('/', async(req, res) => {
+
+});
+
+router.get('/:id', async(req, res) => {
+
+});
+
+router.patch('/:id', async(req, res) => {
+
+});
+
+router.delete('/:id', async(req, res) => {
+
+});
+
+router.get('/:id/students', async(req, res) => {
+
+});
+
+router.post('/:id/students', async(req, res) => {
+
+});
+
+router.get('/:id/roster', async(req, res) => {
+
+});
+
+router.get('/:id/assignments', async(req, res) => {
+
+});
