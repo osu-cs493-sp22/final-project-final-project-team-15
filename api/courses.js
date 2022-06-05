786d87ace74d7d073657a047ed17436a666bc289
NawafAlOthman <72106212+NawafAlOthman@users.noreply.github.com>
2022-06-04 20:58:30 -0700

    get/getbyId/getAssignments are done

--- api/courses.js
+++ api/courses.js
@@ -1,53 +1,102 @@
-const router = require('express').Router();
-const { validateAgainstSchema, extractValidFields } = require('../lib/validation');
+const router = require("express").Router();
+const {
+  validateAgainstSchema,
+  extractValidFields,
+} = require("../lib/validation");
 
-const CourseSchema = require('../models/courses')
-const { users } = require('./users');
-const { assignments } = require('./assingments');
-const { submissions } = require('./submissions');
+const CourseSchema = require("../models/courses");
+const { users } = require("./users");
+const { assignments } = require("./assingments");
+const { submissions } = require("./submissions");
 
-const {generateAuthToken, requireAuthentication} = require('../lib/auth')
-const { getDbInstance } = require('../lib/mongo')
+const { generateAuthToken, requireAuthentication } = require("../lib/auth");
+const { getDbInstance } = require("../lib/mongo");
 
-const { ObjectID, ListCollectionsCursor, ObjectId } = require('mongodb');
-const e = require('express');
+const { ObjectID, ListCollectionsCursor, ObjectId } = require("mongodb");
+const e = require("express");
 
 exports.router = router;
 exports.courses = courses;
 
+async function getCoursesPage(page) {
+  const db = getDbInstance();
+  const collection = db.collection("courses");
+  const count = await collection.countDocuments();
+  const pageSize = 10;
+  const lastPage = Math.ceil(count / pageSize);
+  page = page < 1 ? 1 : page;
+  const offset = (page - 1) * pageSize;
+  const results = await collection
+    .find({})
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(pageSize)
+    .toArray();
 
-router.get('/', async(req, res) => {
+  return {
+    courses: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: pageSize,
+    count: count,
+  };
+}
 
+async function getCourseById(id) {
+  const db = getDbInstance();
+  const collection = db.collection("courses");
+  console.log(id);
+  const courses = await collection
+    .aggregate([{ $match: { _id: new ObjectId(id) } }])
+    .toArray();
+  console.log(courses);
+  return courses[0];
+}
+
+async function getAssignmentsByCourseId(id) {
+  const db = getDbInstance();
+  const collection = db.collection("assignments");
+  console.log(id);
+  const assignments = await collection
+    .aggregate([{ $match: { courseId: new ObjectId(id) } }])
+    .toArray();
+  console.log(assignments);
+  return assignments;
+}
+
+router.get("/", async (req, res) => {
+  const coursesPage = await getCoursesPage(parseInt(req.query.page) || 1);
+  res.status(200).send(coursesPage);
 });
 
-router.post('/', requireAuthentication, async(req, res) => {
+router.post("/", requireAuthentication, async (req, res) => {});
 
+router.get("/:id", async (req, res) => {
+  const id = req.params.businessid;
+  const course = await getCourseById(id);
+  if (course) {
+    res.status(200).send(course);
+  } else {
+    next();
+  }
 });
 
-router.get('/:id', async(req, res) => {
+router.patch("/:id", requireAuthentication, async (req, res) => {});
 
-});
+router.delete("/:id", requireAuthentication, async (req, res) => {});
 
-router.patch('/:id', requireAuthentication, async(req, res) => {
+router.get("/:id/students", requireAuthentication, async (req, res) => {});
 
-});
+router.post("/:id/students", requireAuthentication, async (req, res) => {});
 
-router.delete('/:id', requireAuthentication, async(req, res) => {
+router.get("/:id/roster", requireAuthentication, async (req, res) => {});
 
-});
-
-router.get('/:id/students', requireAuthentication, async(req, res) => {
-
-});
-
-router.post('/:id/students', requireAuthentication, async(req, res) => {
-
-});
-
-router.get('/:id/roster', requireAuthentication, async(req, res) => {
-
-});
-
-router.get('/:id/assignments', async(req, res) => {
-
+router.get("/:id/assignments", async (req, res) => {
+  const id = req.params.businessid;
+  const assignments = await getAssignmentsByCourseId(id);
+  if (assignments) {
+    res.status(200).send(assignments);
+  } else {
+    next();
+  }
 });
